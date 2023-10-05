041d847707dd92ed0858916ec240bae9f026758d
Janne Lehikoinen <janne@lehi.fi>
2023-10-05 16:12:57 +0200

    Sonoma requires location privileges

--- SwiftUIMenuBarAppDemo/SwiftUIMenuBarAppDemoApp.swift
+++ SwiftUIMenuBarAppDemo/SwiftUIMenuBarAppDemoApp.swift
@@ -5,6 +5,13 @@
 //  Created by Janne Lehikoinen on 16.3.2023.
 //
 
+/* TODO:
+ 
+ - https://developer.apple.com/forums/thread/732431
+ - SSID information is not available unless Location Services is enabled and the user has authorized the calling app to use location services.
+ 
+ */
+
 import SwiftUI
 
 @main
@@ -12,8 +19,8 @@ struct SwiftUIMenuBarAppDemoApp: App {
     var body: some Scene {
         MenuBarExtra("Wi-Fi Info", systemImage: "wifi.square") {
             // MenuBarList()
-            MenuBarWindow()
-            // MoreGaugesView()
+//            MenuBarWindow()
+            MoreGaugesView()
         }
         // .menuBarExtraStyle(.menu)
         .menuBarExtraStyle(.window)
